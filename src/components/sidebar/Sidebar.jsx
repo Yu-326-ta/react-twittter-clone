894f93046059b9a1037652d0722ed53d5b31d842
Yu-326-ta <yoshinagayuuta@outlook.jp>
2022-10-04 10:02:59 +0900

    material uiインストール

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <TwitterIcon className="sidebar_twitterIcon" />
+    </div>
+  );
+};
+export default Sidebar;
