d17433dedbfa8e4a9dfab66010719fb5a5f54b4c
Yu-326-ta <yoshinagayuuta@outlook.jp>
2022-10-07 08:02:31 +0900

    firebaseのインストール

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD27nv-P98m9g2E7oxDRnlCrNI4nxiJ6so",
+    authDomain: "twitter-clone-b1c1e.firebaseapp.com",
+    projectId: "twitter-clone-b1c1e",
+    storageBucket: "twitter-clone-b1c1e.appspot.com",
+    messagingSenderId: "892649479236",
+    appId: "1:892649479236:web:ac5969807f4e877500b011"
+  };
+
+  const app = initializeApp(firebaseConfig);
